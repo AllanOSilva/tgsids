d4b0eb627fafeead1bed40ae72dbb0f233adf0b2
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-09 00:14:13 -0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
-const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const watch = require('gulp-watch');
 
-function comprimeImagens(){
-    return gulp.src('./src/imagens/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/imagens'));
-}
-
+// Tarefa para compilar arquivos Less para CSS
 gulp.task('less', function() {
     return gulp.src('src/styles/*.less')
         .pipe(less())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/styles'));
 });
 
-exports.default = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-}
+// Tarefa para otimizar imagens
+gulp.task('imagemin', function() {
+    return gulp.src('src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/imagens'));
+});
+
+// Tarefa para assistir mudanças nos arquivos Less e imagens
+gulp.task('watch', function() {
+    gulp.watch('src/styles/*.less', gulp.series('less'));
+    gulp.watch('src/imagens/**/*', gulp.series('imagemin'));
+});
+
+// Tarefa padrão do Gulp
+gulp.task('default', gulp.series('less', 'imagemin', 'watch'));
