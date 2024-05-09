85a7ec3b3b56713e25cf6e1ca21e82eecb3ffc18
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-08 23:31:14 -0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens(){
+    return gulp.src('./src/imagens/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens'));
+}
 
 gulp.task('less', function() {
     return gulp.src('src/styles/*.less')
         .pipe(less())
         .pipe(gulp.dest('dist/css'));
 });
+
+exports.default = function(){
+   
+    gulp.watch('./source/scripts/*.js', {ignoreInitial:false}, gulp.series(comprimeJavascript));
+    gulp.watch('./source/images/*', {ignoreInitial:false}, gulp.series(comprimeImagens));
+}
