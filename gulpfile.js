de8549fd9ae83a09c6961f202fe890fcb96b1796
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-09 00:21:40 -0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
-const imagemin = require('gulp-imagemin');
-const watch = require('gulp-watch');
+
 
 // Tarefa para compilar arquivos Less para CSS
 gulp.task('less', function() {
@@ -10,18 +9,13 @@ gulp.task('less', function() {
         .pipe(gulp.dest('dist/styles'));
 });
 
-// Tarefa para otimizar imagens
-gulp.task('imagemin', function() {
-    return gulp.src('src/imagens/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/imagens'));
-});
+
 
 // Tarefa para assistir mudanças nos arquivos Less e imagens
 gulp.task('watch', function() {
     gulp.watch('src/styles/*.less', gulp.series('less'));
-    gulp.watch('src/imagens/**/*', gulp.series('imagemin'));
+    
 });
 
 // Tarefa padrão do Gulp
-gulp.task('default', gulp.series('less', 'imagemin', 'watch'));
+gulp.task('default', gulp.series('less'));
