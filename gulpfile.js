a69cb1c515cbda943305b276d5327ed914c2be3f
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-10 13:38:08 -0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,15 @@ gulp.task('less', function() {
         .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/styles/*.less', gulp.series('less'));
-});
+
 gulp.task('minify-html', function() {
     return gulp.src('src/*.html')
       .pipe(htmlmin({ collapseWhitespace: true }))
       .pipe(gulp.dest('dist'));
   });
+  gulp.task('watch', function() {
+    gulp.watch('src/styles/*.less', gulp.series('less'));
+    gulp.watch('src/*.html', gulp.series('minify-html'))
+});
 
 gulp.task('default', gulp.series('minify-html','less', 'watch'));
