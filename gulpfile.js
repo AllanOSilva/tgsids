f96624aad2e0e0716c7c53194e8b7a2ace5c7a45
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-10 12:18:55 -0300

    up

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 
 const gulp = require('gulp');
 const less = require('gulp-less');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('less', function() {
     return gulp.src('src/styles/main.less')
@@ -11,5 +12,10 @@ gulp.task('less', function() {
 gulp.task('watch', function() {
     gulp.watch('src/styles/*.less', gulp.series('less'));
 });
+gulp.task('minify-html', function() {
+    return gulp.src('src/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+  });
 
-gulp.task('default', gulp.series('less', 'watch'));
+gulp.task('default', gulp.series('minify-html','less', 'watch'));
