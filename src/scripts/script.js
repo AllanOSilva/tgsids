d7013773a64196caefdef219a0cdf36256d8810c
AllanOSilva <154864238+AllanOSilva@users.noreply.github.com>
2024-05-17 19:52:07 -0300

    up

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,9 @@
 function scrollToSection(event, sectionId) {
     event.preventDefault(); // Previne a ação padrão do link
     const element = document.getElementById(sectionId);
-    const headerOffset = 20; // Espaço adicional de 20px
+    const headerOffset = 130; // Espaço adicional de 20px
     const elementPosition = element.getBoundingClientRect().top;
-    //const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
 
     window.scrollTo({
         top: offsetPosition,
